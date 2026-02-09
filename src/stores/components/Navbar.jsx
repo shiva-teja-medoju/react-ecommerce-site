018d8e74db5abdf4d1600557d32439a865e02629
shivatej2303 <shivatejamedoju@gmail.com>
2026-02-09 15:27:28 +0530

    Fix filename casing for Banner.png

--- src/stores/components/Navbar.jsx
+++ src/stores/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
-import Banner from '../../assets/Banner.png';
+import Banner from '../../assets/banner.png';
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setShowCart } from "../../redux/cartSlice";
